0b8b594daa594862ae39dcc39a58029d5a13561a
Ahmed-momo <am3404752@gmail.com>
2024-06-24 17:28:36 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,21 +1,21 @@
-function toggleResetBtn(){
-    const resetBtn = document.querySelector('.reset')
-    resetBtn.classList.toggle('active')
+     function toggleResetBtn(){
+      const resetBtn = document.querySelector('.reset')
+      resetBtn.classList.toggle('active')
 }
 
 function switchBanner(name){
-    const banner = document.querySelector('#banner');
-    if(!banner.classList.contains(name)){
-          banner.className = 'banner';
-          banner.classList.add(name);
-          toggleResetBtn();
-    }
-    return;
-    
+      const banner = document.querySelector('#banner');
+      if(!banner.classList.contains(name)){
+            banner.className = 'banner';
+            banner.classList.add(name);
+            toggleResetBtn();
+      }
+      return;
+      
 }
 
 function resetBanner(){
-    const banner = document.querySelector('#banner');
-    banner.className = 'banner';
-    toggleResetBtn();
+      const banner = document.querySelector('#banner');
+      banner.className = 'banner';
+      toggleResetBtn();
 }
